f40d1b4bf236c6f6606dcb07abb3fa0e01d99a06
MuhdHanish <muhammedhanish11@gmail.com>
2023-07-10 15:01:01 +0530

    basic setup completed include the database connecting , server listen, cors setting and the clean arch folder structure created

--- index.ts
+++ index.ts
@@ -1,12 +1,35 @@
+// import dependencies
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 
+// import dotenv
+import { config } from "dotenv";
+config();
+
+// database connection
+import connnectDatabase from "./src/framework/database/config/dbConfig";
+
+// creat express application
 const app = express();
+
+// cors setting
+app.use(
+  cors({
+    origin: ["http://localhost:5173", process.env.CORS_ORIGIN_URL as string ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
+// database connecting & app listen
 const port = process.env.PORT || 3000;
-app.listen(port, (): void => console.log(`Server running...`));
-
+connnectDatabase()
+  .then((res) => {
+    console.log(res);
+    app.listen(port, (): void => console.log(`Server running...`));
+  })
+  .catch((error) => console.log(`Failed to connect database`, error));
