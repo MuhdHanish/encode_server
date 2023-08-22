bd9d04478037a44165ba6f9e57dbbd4f26e0b7f6
MuhdHanish <muhammedhanish11@gmail.com>
2023-08-22 14:47:40 +0530

    added the use case

--- src/app/usecases/user/usersCases.ts
+++ src/app/usecases/user/usersCases.ts
@@ -1,32 +1,32 @@
 import { User } from "../../../domain/models/User";
 import { userRepository } from "../../../framework/repository/userRepository";
 
-export const getUsers = (userRepository: userRepository) => async ():Promise<User[]|null> => {
+export const getUsers = (userRepository: userRepository) => async (): Promise<User[] | null> => {
   const users = await userRepository.getUsers();
   if (users) {
     return users;
   } else {
     return null;
   }
-}
+};
 
-export const getUsersByRole = (userRepository: userRepository) => async (role:string):Promise<User[]|null> => {
+export const getUsersByRole = (userRepository: userRepository) => async (role: string): Promise<User[] | null> => {
   const users = await userRepository.getUsersByRole(role);
   if (users) {
     return users;
   } else {
     return null;
   }
-}
+};
 
-export const getUsersCount = (userRepository: userRepository) => async ():Promise<number|null> => {
+export const getUsersCount = (userRepository: userRepository) => async (): Promise<number | null> => {
   const count = await userRepository.getUsersCount();
   if (count) {
     return count;
   } else {
     return null;
   }
-}
+};
 
 export const getUsersCountByRole = (userRepository: userRepository) => async(role:string):Promise<number|null> => {
   const count = await userRepository.getUsersCountByRole(role);
@@ -37,22 +37,40 @@ export const getUsersCountByRole = (userRepository: userRepository) => async(rol
   }
 }
 
-export const blockUser = (userRepository: userRepository) => async(userId:string):Promise<User|null> => {
+export const blockUser = (userRepository: userRepository) => async (userId: string): Promise<User | null> => {
   const user = await userRepository.blockUser(userId);
   if (user) {
     return user;
   } else {
     return null;
   }
-}
+};
 
 
-export const unBlockUser = (userRepository: userRepository) => async(userId:string):Promise<User|null> => {
+export const unBlockUser = (userRepository: userRepository) => async (userId: string): Promise<User | null> => {
   const user = await userRepository.unBlockUser(userId);
   if (user) {
     return user;
   } else {
     return null;
   }
-}
+};
+
+export const editProfileImage = (userRepository: userRepository) => async (id: string, url: string): Promise<User | null> => {
+  const user = await userRepository.updateProfileImage(id, url);
+  if (user) {
+    return user;
+  } else {
+    return null;
+  }
+};
+
+export const editCredentials = (userRepository: userRepository) => async (id: string, email: string, username:string): Promise<User | null> => {
+  const user = await userRepository.updateCredentials(id, email, username);
+  if (user) {
+    return user;
+  } else {
+    return null;
+  }
+};
 
